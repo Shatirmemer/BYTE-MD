b2a468817a623c76fe7a55c34ceeb08775bacecb
Shatirmemer <146601932+Shatirmemer@users.noreply.github.com>
2024-08-07 22:45:44 +0500

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaNRcHSJP
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://raw.githubusercontent.com/HyHamza/HyHamza/main/Images/logo.jpg";
 global.devs = "923072380380";
 global.sudo = process.env.SUDO || "923072380380";
-global.owner = process.env.OWNER_NUMBER || "923072380380";
+global.owner = process.env.OWNER_NUMBER || "923006838210";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -32,7 +32,7 @@ global.waPresence = process.env.WAPRESENCE || "available";
 global.readcmds = process.env.READ_COMMAND || "true";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
-global.read_status = process.env.AUTO_READ_STATUS || "false";
+global.read_status = process.env.AUTO_READ_STATUS || "true";
 global.save_status = process.env.AUTO_SAVE_STATUS || "false";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "all";
 global.read_status_from = process.env.READ_STATUS_FROM || "all";
@@ -57,7 +57,7 @@ global.SESSION_ID =
 
 
   
-  "Your session ID" //Make sure session id starts with Byte;;;
+  "Byte;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNkJJM2RSZDRadm1tTi9RTUZqU1JhNTZha3JJYk91T0kxc3FaY29waTBtWT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicUlySGVZUTVwb09uN1JaN2Q4aTFtbUptRHNmeWhGdlJhM0lLV1M2Z0pCWT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIwS1lleGNpVktjS3YzWnkzVmpBTDBjbWx1RDdXczBxSk4xY0NoSHp1VEVBPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJsVkNuYVpEWWdyQ1BnT0Y5djNtK1d4c0JJWXhTbkNHcXBab0l0dWdUV25VPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkdINTlCUVZSLzN5Y3RRUkg2QW1oTVNMaUNFSVYxSXZCNlVMM1BoZ2dxV3c9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlBydjJQekFLY2lXckRTb09LN1VFVmRoMk53bmVFWGd3NVJWNzBZWWRsV0k9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiWUlUWFRudEpJMTArNTBDS0RtRnhXYkpMR09DTFE3d3dSUDZVaFlvK1Btaz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiaThXUlJuT3BWanIzVE5Xb29RdG9aNHA3MDJXcnBjK0R6TzVZYlUzRThGQT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImkvK3VXVTFhYzl2LzFjbm1YbS9CTm1RbTRvL0JKUzArT2pxMU0xVVhlNDVQVWlVbVVBYVJGLzVIcTYzSTV0UGN6R01rTGxPMEphWGg0dVl3NUFETER3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjEyLCJhZHZTZWNyZXRLZXkiOiJRbGp1Nm8yOWVOQUZtMjF2MG5WYUJSd1pKUGxmZlJmZkhTbUYxL0FoM1dJPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJCNndzTWZmLVFRYTFQZEMzSGJNdmVnIiwicGhvbmVJZCI6IjcwZjAyNGRjLTY4OTMtNGFmMy05YjZlLTc2YzVkY2VjMTA4YyIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJtT2x6ditYMnhtWWFHNEpTa3kzOHpqc1BOcGc9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZ3ZJS2xhaVVmaUVabzdYZE8wWVkwN1NCY2drPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IlpKRjhLMkE2IiwibWUiOnsiaWQiOiI5MjMwMDY4MzgyMTA6MjVAcy53aGF0c2FwcC5uZXQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ09uRS92c0JFTC9qenJVR0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6IklkeEtJWW1XNk1VRjZTVm5jUUpOa0FzczE5ZmVIdU1yU0xsVHZYZSsybEE9IiwiYWNjb3VudFNpZ25hdHVyZSI6IlNxeGg4c0V5S0VjSG5BUUZBcmdYU3ZWRnBCYVltRTJmYXBLQ1M2STdobG9GcFEya0NZOS9pZEJXZnk5eHNpVlErclJRS2JXV3ZnWmFwcFhSRTRFZEFRPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJKbzFXS1hJUDhkZ21WcVRzOVc3bGxTdDhMNGJMTWJYTEFGT1ZFRDg0eWVnNDZaMFFRNDkzbmtpTUtyY0E3clFQUm1WUUM0MU1yTVpNYkpkRlZKTWpBUT09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjkyMzAwNjgzODIxMDoyNUBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJTSGNTaUdKbHVqRkJla2xaM0VDVFpBTExOZlgzaDdqSzBpNVU3MTN2dHBRIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzIzMDUyNDkxLCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQU4zTiJ9" //Make sure session id starts with Byte;;;
 
 
 
@@ -81,7 +81,7 @@ module.exports = {
   caption: process.env.CAPTION || "*Powered By TalkDrove*",
   author: process.env.PACK_AUTHER || "TalkDrove",
   packname: process.env.PACK_NAME || "♥️",
-  botname: process.env.BOT_NAME || "BYTE-MD",
+  botname: process.env.BOT_NAME || "FAISAL-MD",
   ownername: process.env.OWNER_NAME || "TalkDrove",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
@@ -92,9 +92,9 @@ module.exports = {
   antilink_values: process.env.ANTILINK_VALUES || "all",
   HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
   aitts_Voice_Id: process.env.AITTS_ID || "37",
-  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
+  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "sk_251725daa233ab99ebccc99534475b1018becd2de8665e2d",
   WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
-  LANG: (process.env.THEME || "BYTE").toUpperCase(),
+  LANG: (process.env.THEME || "FAISAL").toUpperCase(),
 };
 global.rank = "updated";
 global.isMongodb = false;
